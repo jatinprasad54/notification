d5a2e2009b0c34f40831d63d09aca2bd4358d9a1
jatinprasad54 <jatinprasad54@gmail.com>
2024-10-08 22:34:34 +0530

    Add notification functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useNotifications } from "./useNotifications";
+import { toastReducer } from "./toastReducer";
+import { useReducer } from "react";
+import Toast from "./Toast";
+//useNotifications customHooks return showNotification function
+// showNotification(type , message) type: 4 types
+// error message will disappear in 2 seconds
+// if hovered over it timer will stop
+// multiple notification is allowed at once.
 
+// const useNotifications = () => {
+//   return {
+//     showNotification: ({ type, message }) => {
+//       console.log(type, message);
+//     },
+//   };
+// };
 function App() {
+  const { showNotification } = useNotifications();
+  const [toasts, dispatch] = useReducer(toastReducer, []);
+  const getColor = (type) => {
+    switch (type) {
+      case "SUCCESS":
+        return "green";
+      case "WARNING":
+        return "yellow";
+      case "ERROR":
+        return "red";
+      case "INFO":
+        return "blue";
+      default:
+        return "white";
+    }
+  };
+  const handleClick = (e) => {
+    const { target } = e;
+    const type = target.getAttribute("data-type");
+
+    showNotification({
+      type,
+      toasts,
+      message: `${type} Notification`,
+      dispatch,
+      bgColor: getColor(type),
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1>Add Notification</h1>
+      <div className="actions">
+        <button className="info" onClick={handleClick} data-type="INFO">
+          Info
+        </button>
+        <button className="success" onClick={handleClick} data-type="SUCCESS">
+          Success
+        </button>
+        <button className="warning" onClick={handleClick} data-type="WARNING">
+          Warning
+        </button>
+        <button className="error" onClick={handleClick} data-type="ERROR">
+          Error
+        </button>
+      </div>
+      <Toast toastList={toasts} dispatch={dispatch} />
     </div>
   );
 }
